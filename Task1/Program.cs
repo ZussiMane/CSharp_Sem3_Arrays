711ded56e4be81c60bfdecc0c89436d758eaac27
ZussiMane <igoryunja@gmail.com>
2024-04-26 10:41:36 +0300

    Добавил решение задачи 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,24 @@
 // Примеры
 // [1 3 4 19 3], 8 => Нет
 // [-4 3 4 1], 3 => Да
+
+             0  1  2  3   4 - индексы
+int[] array = { 1, 3, 4, 19, 3 };
+int num = 19;
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break;
+        // Console.WriteLine("Да");
+        // break; //-- оператор прерывания цикла (оператора "if")
+    }
+}
+Console.WriteLine(isExistNum ? "Да" : "Нет");
+
+// if (isExistNum) Console.WtiteLine("Да");
+// else 
+// Console.WriteLine("Нет")
